4569b5b5d4f6afb41e1dd1b7f4e4a9354aef012f
Bram Kok <bram@bramkok.com>
2018-01-13 00:05:27 +0100

    Add eslint config

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    'node': true,
+    'browser': true,
+    'es6': true
+  },
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'indent': ['error', 2],
+    'semi': ['error', 'never'],
+    'curly': ['error', 'multi-line'],
+    "node/no-unpublished-require": ["error", {
+      "allowModules": ['react', 'react-dom']
+    }]
+  }
+}

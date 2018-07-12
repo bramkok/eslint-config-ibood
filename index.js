3ed4933d9bbec353a9733e52de7423aa7d3a1b11
Bram Kok <bram@bramkok.com>
2018-07-12 10:59:15 +0200

    Add jestPuppeteer to globals

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+/* eslint indent: [2, 2] */
+/* eslint semi: ["error", "never"] */
+
 module.exports = {
   env: {
-    'node': true,
-    'browser': true,
-    'es6': true
+    node: true,
+    browser: true,
+    es6: true,
+    jest: true
   },
-  'extends': ['xo-react'],
+  extends: ['xo-react'],
   rules: {
     'object-curly-spacing': ['error', 'always'],
-    'indent': ['error', 2],
-    'semi': ['error', 'never'],
-    'curly': ['error', 'multi-line'],
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    curly: ['error', 'multi-line'],
     'node/no-unpublished-require': 0,
     'node/no-unpublished-import': 0,
     'react/jsx-indent': [2, 2],
@@ -17,27 +21,21 @@ module.exports = {
       ignorePattern: '^((?!(abc|def)).)*$',
       ignoreInlineComments: true,
       ignoreConsecutiveComments: true
-    }],
+    }]
   },
   settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx"
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx'
         ]
       }
     }
   },
   globals: {
-    afterAll: false,
-    afterEach: false,
-    beforeAll: false,
-    beforeEach: false,
-    describe: false,
-    test: false,
-    it: false,
-    expect: false,
-    jest: false
+    page: true,
+    browser: true,
+    jestPuppeteer: true
   }
 }
